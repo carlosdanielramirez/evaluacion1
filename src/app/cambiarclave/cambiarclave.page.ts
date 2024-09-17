d08083fe0209da7f941e8d19101adc15c9a45247
daniela <dan.fariaso@duocuc.cl>
2024-09-17 19:34:31 -0300

    avance cambio clave

--- src/app/cambiarclave/cambiarclave.page.ts
+++ src/app/cambiarclave/cambiarclave.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cambiarclave',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cambiarclave.page.scss'],
 })
 export class CambiarclavePage implements OnInit {
+  claveActual = '';
+  nuevaClave = '';
+  confirmarClave = '';
 
-  constructor() { }
+  claveAlmacenada = 'MiClav3';
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async cambiarClave() {
+    if (this.claveActual !== this.claveAlmacenada) {
+      const alert = await this.alertController.create({
+        message: 'La clave actual no es correcta.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    if (this.nuevaClave !== this.confirmarClave) {
+      const alert = await this.alertController.create({
+        message: 'La nueva clave y la confirmación no coinciden.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+    if (this.nuevaClave = this.confirmarClave) {
+      const alert = await this.alertController.create({
+        message: 'La clave ha sido cambiada exitosamente.',
+        buttons: ['OK']
+      });
+      await alert.present();
+
+    }
+
+   
   }
 
 }
